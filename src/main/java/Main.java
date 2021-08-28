336d1986e9bd81abe5f45375a4d44666b3acec4f
Swaggeroo <mugel849@gmail.com>
2021-08-28 17:07:02 +0200

    Comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main {
     }
 
     public Main() {
+        //read files in input
         File[] dir = new File("input/").listFiles();
         if (dir != null) {
             int count = 0;
@@ -34,27 +35,33 @@ public class Main {
             }
         }
 
-
+        //Generate a date String for output filename
         Date date = Calendar.getInstance().getTime();
         DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd-hhmmss");
         String strDate = dateFormat.format(date);
 
+        //process every picture
         for (int i = 0; i < images.size(); i++){
             try {
                 BufferedImage bufferedImage = ImageIO.read(images.get(i));
+                //get sizes for 16:9
                 float width = bufferedImage.getWidth();
                 int height = (int)((width/16)*9);
+                //if picture is wider than 16:9
                 if (height < bufferedImage.getHeight()){
+                    //crop image in center
                     bufferedImage = bufferedImage.getSubimage(0,(bufferedImage.getHeight()-height)/2,bufferedImage.getWidth(),height);
                 }else {
+                    //recalculate sizes
                     height = bufferedImage.getHeight();
                     int newWidth = (int) ((((float) height)/9)*16);
+                    //crop image in center
                     bufferedImage = bufferedImage.getSubimage((bufferedImage.getWidth()-newWidth)/2,0,newWidth,height);
                 }
 
                 BufferedImage instaPic = putOnPlainWhite(bufferedImage);
-                //showImage(instaPic);
 
+                //Save new picture
                 File outputfile = new File("./output/insta"+strDate+"-"+(i+1)+".png");
                 ImageIO.write(instaPic, "png", outputfile);
                 System.out.println((i+1) + "/" + images.size() + "\t\t" + outputfile.getName()+"\tCreated! ");
@@ -64,29 +71,21 @@ public class Main {
         }
     }
 
-    private BufferedImage getScaledImage(BufferedImage srcImg, int w, int h){
-        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = resizedImg.createGraphics();
-
-        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g2.drawImage(srcImg, 0, 0, w, h, null);
-        g2.dispose();
-
-        return resizedImg;
-    }
-
     private BufferedImage putOnPlainWhite(BufferedImage bufferedImage){
+        //make an empty square image
         BufferedImage instaPic = new BufferedImage(bufferedImage.getWidth(),bufferedImage.getWidth(), BufferedImage.TYPE_INT_ARGB);
 
         Graphics2D g2 = instaPic.createGraphics();
 
         g2.setColor(Color.WHITE);
         g2.fillRect(0, 0, instaPic.getWidth(), instaPic.getHeight());
+        //put image in center
         g2.drawImage(bufferedImage,0,((bufferedImage.getWidth()- bufferedImage.getHeight())/2),null);
         g2.dispose();
         return instaPic;
     }
 
+    //showing processed image in Window (method for debugging)
     private void showImage(BufferedImage bufferedImage){
         JLabel picLabel = new JLabel(new ImageIcon(getScaledImage(bufferedImage,800,800)));
         JPanel jPanel = new JPanel();
@@ -96,5 +95,17 @@ public class Main {
         f.setSize(new Dimension(900,900));
         f.add(jPanel);
         f.setVisible(true);
+    }
+
+    //scale image down for showing in window (method for debugging)
+    private BufferedImage getScaledImage(BufferedImage srcImg, int w, int h){
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImg;
     }
 }
